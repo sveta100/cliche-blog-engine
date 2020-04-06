066481ad2834f62c11d51e7893a80823d00872e3
sveta100 <sveta.borisenkova@hotmail.com>
2020-04-06 22:37:01 +0300

    style fixes

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,24 +1,24 @@
-const webpack = require("webpack");
-const clientConfig = require("../webpack.config");
+const webpack = require('webpack');
+const webpackDev = require('webpack-dev-middleware');
+const webpackHot = require('webpack-hot-middleware');
+const clientConfig = require('../webpack.config');
 
 module.exports = function setUpDevServer(app) {
-  clientConfig.entry.app = [
-    "webpack-hot-middleware/client",
-    clientConfig.entry.app
-  ];
-  clientConfig.plugins.push(
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  );
-  const clientCompiler = webpack(clientConfig);
+	clientConfig.entry.app = [
+		'webpack-hot-middleware/client',
+		clientConfig.entry.app,
+	];
+	clientConfig.plugins.push(
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin(),
+	);
+	const clientCompiler = webpack(clientConfig);
 
-  app.use(
-    require("webpack-dev-middleware")(clientCompiler, {
-      stats: {
-        colors: true
-      }
-    })
-  );
+	app.use(webpackDev(clientCompiler, {
+		stats: {
+			colors: true,
+		},
+	}));
 
-  app.use(require("webpack-hot-middleware")(clientCompiler));
+	app.use(webpackHot(clientCompiler));
 };
