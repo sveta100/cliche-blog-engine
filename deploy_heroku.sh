96deb5bb42dc4c9632d792b4e1111ae568bc37e1
sveta100 <sveta.borisenkova@hotmail.com>
2020-07-12 21:37:36 +0300

    typo fix

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -1,6 +1,6 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
-docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
+docker login --username _ --password=$HEROKU_APP_KEY registry.heroku.com
 heroku container:push svetab/vue-blog-client --app $HEROKU_APP_NAME
 heroku container:release svetab/vue-blog-client --app $HEROKU_APP_NAME
 heroku container:push svetab/vue-blog-nginx --app $HEROKU_APP_NAME
