3e0f431ac5d8f0d42263f6f9cc6883a54c598532
sveta100 <sveta.borisenkova@hotmail.com>
2020-07-12 22:09:21 +0300

    add release to heroku

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -3,3 +3,8 @@ heroku plugins:install @heroku-cli/plugin-container-registry
 docker login --username=$HEROKU_USER --password=$HEROKU_APP_KEY registry.heroku.com
 docker tag svetab/vue-blog-client registry.heroku.com/cliche-blog-app/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web
+docker tag svetab/vue-blog-nginx registry.heroku.com/cliche-blog-app/nginx
+docker push registry.heroku.com/$HEROKU_APP_NAME/nginx
+docker tag svetab/vue-blog-server registry.heroku.com/cliche-blog-app/server
+docker push registry.heroku.com/$HEROKU_APP_NAME/server
+heroku container:release web nginx server -a $HEROKU_APP_NAME .
