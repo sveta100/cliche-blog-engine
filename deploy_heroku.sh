642a0bd6fd5614ca47ba638fcca02a4e26ab4ca8
sveta100 <sveta.borisenkova@hotmail.com>
2020-07-12 21:57:45 +0300

    fixes

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -1,7 +1,5 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
-docker login --username _ --password=$HEROKU_APP_KEY registry.heroku.com
-heroku container:push svetab/vue-blog-client --app $HEROKU_APP_NAME
-heroku container:release svetab/vue-blog-client --app $HEROKU_APP_NAME
-heroku container:push svetab/vue-blog-nginx --app $HEROKU_APP_NAME
-heroku container:release svetab/vue-blog-nginx --app $HEROKU_APP_NAME
+docker login --username=$HEROKU_USER --password=$HEROKU_APP_KEY registry.heroku.com
+docker tag svetab/vue-blog-client registry.heroku.com/cliche-blog-app/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
